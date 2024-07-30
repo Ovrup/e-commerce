0774bfc51d065222aee73993a03e6a63767e1582
Abhirup Bhattacharjee <abhirup@Abhirups-MacBook-Air.local>
2024-07-30 18:21:24 +0530

    .env file added for firebase configs

--- admin/firebase.js
+++ admin/firebase.js
@@ -2,14 +2,24 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
+require("dotenv").config();
+
+const { REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMANIN,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID
+} = process.env
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDnqOdllhZ547uy_NVOl7TrWhhFN9FlOCY",
-    authDomain: "image-cdn-6e16b.firebaseapp.com",
-    projectId: "image-cdn-6e16b",
-    storageBucket: "image-cdn-6e16b.appspot.com",
-    messagingSenderId: "924166360564",
-    appId: "1:924166360564:web:dc8c61ac34f91e6ed57da1"
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMANIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
